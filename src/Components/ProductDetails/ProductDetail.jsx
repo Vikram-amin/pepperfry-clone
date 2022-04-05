16835ac397ee9e66d200d2940c304147ef74027b
YASHV1752 <yashv1752@gmail.com>
2022-04-05 12:27:42 +0530

    product details and product list is ready

--- src/Components/ProductDetails/ProductDetail.jsx
+++ src/Components/ProductDetails/ProductDetail.jsx
@@ -23,13 +23,17 @@ export const ProductDetail = () =>{
                 <h4>Last Day to <a href="">Earn Cashback upto 5%</a></h4>
                 <a href="">EMI option</a>
                 <br />
+                <img className="offer" src="https://ii2.pepperfry.com/media/wysiwyg/banners/Promo_Web_VIPCoupon_2X_01042022_es.jpg" alt="" />
+                <br />
                 <label htmlFor="">DELIVERY &emsp; &emsp; &emsp; &emsp;</label>
-                <span></span>
-                <span></span>
                 <input type="text" placeholder="Enter a Pincode" value={pincode} onChange={(e)=>handlerChange(e)} className="pincode" />
                 <button className="pinBtn">APPLY</button>
                 <h5 className="del">Delivery charges as applicable <b>Pincode Required</b></h5>
                 <h5 className="del">Assembly Charges as applicable <b>Pincode Required</b></h5>
+                <div className="button">
+                    <button className="add">ADD TO CART</button>
+                    <button className="buy">BUY NOW</button>
+                </div>
             </div>
         </div>
         </>
