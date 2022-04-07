56d81262c656f8e755e762b4579f40992529b74c
YASHV1752 <yashv1752@gmail.com>
2022-04-07 22:39:00 +0530

    main poduct detail and routing edited

--- src/Routes/Routing.jsx
+++ src/Routes/Routing.jsx
@@ -6,11 +6,7 @@ import { MainCart } from '../Components/Cart/MainCart.jsx';
 import { MainCheckout } from '../Components/Checkout/MainCheckout';
 import { MainProductDetail } from '../Components/ProductDetails/MainProductDetail';
 import MainProductList from '../Components/ProductList/MainProductList';
-<<<<<<< HEAD
-import { MainAddress } from '../Components/Address/MainAddress';
-=======
 import { MainAddress } from "../Components/Address/MainAddress";
->>>>>>> 039035f31ed13b2ffa388c8f4b270e7786336e71
 
 
 
@@ -21,11 +17,6 @@ const Routing = () => {
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<MainCart />} />
         <Route path="/productlist" element={<MainProductList />} />
-<<<<<<< HEAD
-        <Route path="/checkout" element={<MainCheckout />} />
-        <Route path="/address" element={<MainAddress />} />
-=======
->>>>>>> 039035f31ed13b2ffa388c8f4b270e7786336e71
         <Route path="/productlist/:id" element={<MainProductDetail />} />
         <Route path="/address" element={<MainAddress />} />
         <Route path="/checkout" element={<MainCheckout />} />
