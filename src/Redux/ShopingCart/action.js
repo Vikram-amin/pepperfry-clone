cfd7d974ddbb25b26446335b3dacda9414f95ff4
Vikram <vikramamin22@gmail.com>
2022-04-07 23:23:03 +0530

    changes done in app.css

--- src/Redux/ShopingCart/action.js
+++ src/Redux/ShopingCart/action.js
@@ -8,22 +8,22 @@ export const addToCart = (itemID) => ({
   }
 });
 
-export const removeFromCart = (itemID) => ({
-  type: REMOVE_FROM_CART,
-  payload: {
-    id: itemID,
-  },
-});
+// export const removeFromCart = (itemID) => ({
+//   type: REMOVE_FROM_CART,
+//   payload: {
+//     id: itemID,
+//   },
+// });
 
-export const adjustQuenty = (itemID,value) => {
-    return {
-      type: ADJUST_QUT,
-      payload: {
-        id: itemID,
-        quantity : value,
-      },
-    };
-}
+// export const adjustQuenty = (itemID,value) => {
+//     return {
+//       type: ADJUST_QUT,
+//       payload: {
+//         id: itemID,
+//         quantity : value,
+//       },
+//     };
+// }
 
 export const loadCurrentItem = (item) => ({
   type : LOAD_CURRENT_ITEM,
