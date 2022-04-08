056834d11aaace54f503652405a520bd17b9f7f6
Vikram <vikramamin22@gmail.com>
2022-04-08 07:57:21 +0530

    functanality is done for product List and product detail page

--- src/Redux/Product/product-action.js
+++ src/Redux/Product/product-action.js
@@ -8,9 +8,25 @@ export const addToCart = (itemID) =>{
         }
     }
 }
-export const loadCurrentItem = (itemID) =>{
-    return{
-        type: actionTypes.LOAD_CURRENT_ITEM,
-        payload: itemID
-    }
+
+export const removeFromCart = (itemID) => ({
+  type: actionTypes.REMOVE_FROM_CART,
+  payload: {
+    id: itemID,
+  },
+});
+
+export const adjustQuenty = (itemID,value) => {
+    return {
+      type: actionTypes.ADJUST_QUT,
+      payload: {
+        id: itemID,
+        quantity : value,
+      },
+    };
 }
+
+export const loadCurrentItem = (item) => ({
+  type : actionTypes.LOAD_CURRENT_ITEM,
+  payload : item,
+});
